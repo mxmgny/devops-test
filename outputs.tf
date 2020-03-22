b588cdacadc06e4488f500995b5ea12e4528c320
Maksym Hnoinskyi <maksym.hnoinskyi@preen.me>
2020-03-22 22:36:32 +0200

    Complete

--- outputs.tf
+++ outputs.tf
@@ -49,6 +49,80 @@ users:
         - "${var.cluster-name}"
 KUBECONFIG
 }
+######
+
+
+locals {
+  fluentd = <<FLUENTD
+
+
+
+apiVersion: extensions/v1beta1
+kind: DaemonSet
+metadata:
+  labels:
+    k8s-app: fluentd-logging
+    kubernetes.io/cluster-service: "true"
+  name: fluentd-elasticsearch
+  namespace: kube-system
+spec:
+  selector:
+    matchLabels:
+      k8s-app: fluentd-logging
+      kubernetes.io/cluster-service: "true"
+  template:
+    metadata:
+      annotations:
+        scheduler.alpha.kubernetes.io/critical-pod: ""
+      creationTimestamp: null
+      labels:
+        k8s-app: fluentd-logging
+        kubernetes.io/cluster-service: "true"
+      name: fluentd-elasticsearch
+      namespace: kube-system
+    spec:
+      containers:
+      - env:
+        - name: AWS_REGION
+          value: ${var.zone}
+        - name: AWS_ELASTICSEARCH_URL
+          value: ${module.es.endpoint}
+        - name: AWS_ACCESS_KEY_ID
+          value: ${var.access_key_id}
+        - name: AWS_SECRET_ACCESS_KEY
+          value: ${var.secret_access_key}
+        image: cheungpat/fluentd-elasticsearch-aws:1.22
+        imagePullPolicy: IfNotPresent
+        name: fluentd-elasticsearch
+        resources:
+          limits:
+            memory: 500Mi
+          requests:
+            cpu: 100m
+            memory: 200Mi
+        volumeMounts:
+        - mountPath: /var/log
+          name: varlog
+        - mountPath: /var/lib/docker/containers
+          name: varlibdockercontainers
+          readOnly: true
+      restartPolicy: Always
+      tolerations:
+      - key: node-role.kubernetes.io/master
+        effect: NoSchedule
+      volumes:
+      - hostPath:
+          path: /var/log
+        name: varlog
+      - hostPath:
+          path: /var/lib/docker/containers
+        name: varlibdockercontainers
+FLUENTD
+}
+
+output "fluendt_deployment" {
+  value = local.fluentd
+}
 
 output "config_map_aws_auth" {
   value = local.config_map_aws_auth
--- variables.tf
+++ variables.tf
@@ -6,3 +6,21 @@ variable "cluster-name" {
   default = "terraform-eks-demo"
   type    = string
 }
+
+variable "access_key_id" {
+  type    = string
+  default = "" # Put here your creds
+}
+
+variable "secret_access_key" {
+  type    = string
+  default = "" # put here your creds
+}
+
+variable "zone" {
+  type    = string
+  default = "us-east-1"
+}
+
+
+
